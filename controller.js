6b8a5c3d0ec4d5dc0d04622002e3fb880e4e4235
nabara99 <rtaufiq212@gmail.com>
2022-08-21 10:50:45 +0800

    menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -11,9 +11,25 @@ exports.index = function (req, res) {
 exports.tampilsemuamahasiwa = function (req, res) {
   connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
     if (error) {
-      connection.log(error);
+      console.log(error);
     } else {
       response.ok(rows, res);
     }
   });
+};
+
+// menampilkan data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function (req, res) {
+  let id = req.params.id;
+  connection.query(
+    "SELECT * FROM MAHASISWA WHERE id_mahasiswa = ?",
+    [id],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
 };
--- routes.js
+++ routes.js
@@ -6,4 +6,6 @@ module.exports = function (app) {
   app.route("/").get(jsonku.index);
 
   app.route("/tampil").get(jsonku.tampilsemuamahasiwa);
+
+  app.route("/tampil/:id").get(jsonku.tampilberdasarkanid);
 };
