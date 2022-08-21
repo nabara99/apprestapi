9a02b86e60b206de6ae5a86df598b6dc0d985b93
nabara99 <rtaufiq212@gmail.com>
2022-08-21 10:33:24 +0800

    menampilkan semua data mahasiwa

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require("./res");
 var connection = require("./koneksi");
 
 exports.index = function (req, res) {
-  response.ok("Application REST API running!");
+  response.ok("Application REST API running!", res);
+};
+
+// menampilkan semua data mahasiswa
+exports.tampilsemuamahasiwa = function (req, res) {
+  connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
 };
--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@
 module.exports = function (app) {
   var jsonku = require("./controller");
 
-  app.route("/");
-  get(jsonku.index);
+  app.route("/").get(jsonku.index);
+
+  app.route("/tampil").get(jsonku.tampilsemuamahasiwa);
 };
